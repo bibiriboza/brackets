a7ea8e76bba9366043239e29e9a8ddda163fd3a2
bibiriboza <leshagomon@gmail.com>
2022-08-02 19:28:41 +0300

    feat: implemention the function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const config = bracketsConfig.map((e) => e.join(""));
+  let newStr = str;
+  while (config.some((e) => newStr.includes(e))) {
+    config.forEach((e) => {
+      while (newStr.includes(e)) newStr = newStr.replace(e, "");
+    });
+  }
+  return !newStr.length ? true : false;
+};
